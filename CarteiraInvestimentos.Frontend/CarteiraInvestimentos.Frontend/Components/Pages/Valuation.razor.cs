5c39c2abab3a120b46bc92c9750b09161ec97f14
Renan Jochem <renanjochem98@outlook.com>
2024-09-19 19:24:05 -0300

    Adicionar calculo simples de valuation a uma tela.

---
+++ CarteiraInvestimentos.Frontend/CarteiraInvestimentos.Frontend/Components/Pages/Valuation.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CarteiraInvestimentos.Frontend.Components.Pages
+{
+    public partial class Valuation : ComponentBase
+    {
+        decimal lpa { get; set; } = 0;
+        decimal vpa { get; set; } = 0;
+
+        decimal resultado { get; set; } = 0;
+
+        public void Calcular()
+        {
+            resultado = (decimal)Math.Sqrt(22.5 * (double)lpa * (double)vpa);
+        }
+    }
+}
